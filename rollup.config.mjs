93bc9a3cba6385047ccb9090865d74509b1ffb59
cuongdq3500888 <cuongdq3500888@gmail.com>
2025-08-30 16:52:23 +0700

    make readme and minimy by esbuild 2017

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,20 @@
 import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import esbuild from 'rollup-plugin-esbuild'
 
 export default {
   input: 'src/index.ts', // Điểm vào từ mã TypeScript. -- phương án build trong tham số lệnh
   external: [], // Không có external deps
   output: [
     {
-      file: 'lib/index.js', // CommonJS
+      file: 'lib/index.js', // CommonJS use: import ... from '...';
       format: 'cjs',
       sourcemap: true, // Tùy chọn: Thêm source map để debug
       exports: 'auto'
     },
     {
-      file: 'lib/index.mjs', // ES Module
+      file: 'lib/index.mjs', // ES Module: use = require('..');
       format: 'esm',
       sourcemap: true,
     },
@@ -33,7 +34,10 @@ export default {
       declarationMap: true, // Tạo .d.ts.map để debug
       rootDir: 'src'
     }), // Biên dịch TypeScript
-
+    esbuild({
+      minify: true,
+      target: 'es2017' // Target ES version phù hợp: ES2017+ cho modern browsers // RN 0.60+
+    })
 
   ],
   external: [], // Chỉ định các phụ thuộc bên ngoài nếu cần (ví dụ: không bundle tslib nếu dùng importHelpers)
